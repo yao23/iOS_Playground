4a74044c90801d3a3220144463f982362a743945
yao <myspiritcrazy@gmail.com>
2016-11-26 13:42:45 -0800

    clean debug code

--- CustomViewControllerStarter/BeginnerCook/PopAnimator.swift
+++ CustomViewControllerStarter/BeginnerCook/PopAnimator.swift
@@ -27,12 +27,9 @@ class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
         let initialFrame = presenting ? originFrame : herbView.frame
         let finalFrame = presenting ? herbView.frame : originFrame
 
-        print("init: \(initialFrame)")
-        print("final: \(finalFrame)")
         let xScaleFactor = presenting ? initialFrame.width / finalFrame.width : finalFrame.width / initialFrame.width
         let yScaleFactor = presenting ? initialFrame.height / finalFrame.height : finalFrame.height / initialFrame.height
 
-        print("x: \(xScaleFactor), y: \(yScaleFactor)")
         let scaleTransform = CGAffineTransform(scaleX: xScaleFactor, y: yScaleFactor)
 
         if presenting {
