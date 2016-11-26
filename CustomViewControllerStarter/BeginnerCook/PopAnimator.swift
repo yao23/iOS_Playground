190504b2dfa218ef3ac2e7c76936064948474f8b
yao <myspiritcrazy@gmail.com>
2016-11-26 12:50:00 -0800

    add a fade transition

--- CustomViewControllerStarter/BeginnerCook/PopAnimator.swift
+++ CustomViewControllerStarter/BeginnerCook/PopAnimator.swift
@@ -9,11 +9,26 @@
 import UIKit
 
 class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    let duration = 1.0
+    var presenting = true
+    var originFrame = CGRect.zero
+
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0
+        return duration
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-
+        let containerView = transitionContext.containerView
+        let toView = transitionContext.view(forKey: .to)!
+        containerView.addSubview(toView)
+        toView.alpha = 0.0
+        UIView.animate(withDuration: duration,
+                animations: {
+                    toView.alpha = 1.0
+                },
+                completion: { _ in
+                    transitionContext.completeTransition(true)
+                }
+        )
     }
 }
