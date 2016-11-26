ff6a5d6ee34adf08dec6db201f574269d4d103cf
yao <myspiritcrazy@gmail.com>
2016-11-26 12:36:05 -0800

    add PopAnimator class

---
+++ CustomViewControllerStarter/BeginnerCook/PopAnimator.swift
@@ -0,0 +1,19 @@
+//
+//  PopAnimator.swift
+//  BeginnerCook
+//
+//  Created by Yao Li on 11/26/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0
+    }
+
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+
+    }
+}
