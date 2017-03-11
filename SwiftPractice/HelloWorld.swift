8081b77958335f746fa345a664c866aa4c667d8f
Yao <myspiritcrazy@gmail.com>
2017-03-10 22:45:31 -0800

    while loop

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -73,3 +73,15 @@ for (kind, numbers) in interestingNumbers {
     }
 }
 print(largest)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+ 
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
