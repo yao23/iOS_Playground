efb0bd15b76950ed19f36de79a0ada6f60fcd78d
Yao <myspiritcrazy@gmail.com>
2017-03-10 22:43:20 -0800

    for loop

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -58,3 +58,18 @@ print(greeting)
 let nickName: String? = nil
 let fullName: String = "John Appleseed"
 let informalGreeting = "Hi \(nickName ?? fullName)"
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
