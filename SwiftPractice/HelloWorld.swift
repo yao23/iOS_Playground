cba761b5c80c2ba458102dac349f64b7daa04420
Yao <myspiritcrazy@gmail.com>
2017-03-10 22:29:39 -0800

    if condition

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -29,3 +29,14 @@ var occupations = [
 occupations["Jayne"] = "Public Relations"
 print(shoppingList)
 print(occupations)
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
