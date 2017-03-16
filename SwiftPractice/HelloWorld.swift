7d43113df26b32eca9d2a526498eb9a8facdecab
Yao <myspiritcrazy@gmail.com>
2017-03-15 22:54:04 -0700

    function return function

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -141,3 +141,12 @@ func returnFifteen() -> Int {
     return y
 }
 returnFifteen()
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
