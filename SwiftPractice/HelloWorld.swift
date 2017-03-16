0dfa7e2a23717c98dcc92ed8a914fa416f3ae4d7
Yao <myspiritcrazy@gmail.com>
2017-03-15 22:55:30 -0700

    function take function as parameter

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -150,3 +150,17 @@ func makeIncrementer() -> ((Int) -> Int) {
 }
 var increment = makeIncrementer()
 increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
