6236931c6c3767fd10b11cd4c936c4df234b3db5
Yao <myspiritcrazy@gmail.com>
2017-03-15 22:57:42 -0700

    function as a special case for closure

--- SwiftPractice/HelloWorld.swift
+++ SwiftPractice/HelloWorld.swift
@@ -164,3 +164,11 @@ func lessThanTen(number: Int) -> Bool {
 }
 var numbers = [20, 19, 7, 12]
 hasAnyMatches(list: numbers, condition: lessThanTen)
+
+numbers.map({ (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+let mappedNumbers = numbers.map({ number in 3 * number })
+print(mappedNumbers)
