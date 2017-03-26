bbcbeb75e9537450feb1800b460e3cb09ec7402e
yao <myspiritcrazy@gmail.com>
2017-03-25 23:38:11 -0700

    fix bug to get distance

--- RestaurantSearch/RestaurantSearch/Restaurant.swift
+++ RestaurantSearch/RestaurantSearch/Restaurant.swift
@@ -18,16 +18,18 @@ class Restaurant: NSObject {
 
         let geometry : [String : Any] = jsonData["geometry"] as! [String : Any]
         let location : [String : Any] = geometry["location"] as! [String : Any]
-        let lat : Double = NumberFormatter().number(from:location["lat"] as! String)!.doubleValue
-        let lng : Double = NumberFormatter().number(from:location["lat"] as! String)!.doubleValue
+        let lat : Double = location["lat"] as! Double
+        let lng : Double = location["lng"] as! Double
+        print("location: \(lat), \(lng)")
         let coordinate0 = CLLocation(latitude: lat, longitude: lng) // target location
         let coordinate1 = CLLocation(latitude: 37.3230, longitude: -122.0322) // Apple headquarter as default location, Cupertino, CA
 
         let distanceInMeters = coordinate0.distance(from: coordinate1) // result is in meters
-        distance = String(distanceInMeters)
+        let distanceInMiles : Float = Float(distanceInMeters / 1609);
+        distance = String(distanceInMiles)
     }
 
     func printInfo() {
-        print("Restaurant: \(name), \(distance) meters far away")
+        print("Restaurant: \(name), \(distance) miles far away")
     }
 }
