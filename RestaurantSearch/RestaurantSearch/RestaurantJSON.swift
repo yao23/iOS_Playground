55298bdcaea1f8bf7242b77becbd4b1039f7a4e0
yao <myspiritcrazy@gmail.com>
2017-03-26 22:59:27 -0700

    Restaurant JSON

--- RestaurantSearch/RestaurantSearch/RestaurantJSON.swift
+++ RestaurantSearch/RestaurantSearch/RestaurantJSON.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import AlamofireObjectMapper
+
+class RestaurantJSON: Mappable {
+    var geometry : Geometry
+
+
+    required init?(map: Map){
+
+    }
+
+    func mapping(map: Map) {
+        name <- map["name"]
+        geometry <- map["geometry"]
+    }
+}
+
+class Geometry: Mappable {
+    var location : Location
+
+    required init?(map: Map){
+
+    }
+
+    func mapping(map: Map) {
+        location <- map["location"]
+    }
+}
+
+class Location: Mappable {
+    let lat : Double
+    let lng : Double
+
+    required init?(map: Map){
+
+    }
+
+    func mapping(map: Map) {
+        lat <- map["lat"]
+        lng <- map["lng"]
+    }
+}
