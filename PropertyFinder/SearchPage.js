2ba1fc868881dff5182e5de7637b83e6d2ff5839
yao <myspiritcrazy@gmail.com>
2017-05-09 22:55:57 -0700

    search page

---
+++ PropertyFinder/SearchPage.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  }
+});
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.exports = SearchPage;
--- PropertyFinder/index.ios.js
+++ PropertyFinder/index.ios.js
@@ -7,6 +7,7 @@
 
 var React = require('react');
 var ReactNative = require('react-native');
+var SearchPage = require('./SearchPage');
 
 var styles = ReactNative.StyleSheet.create({
   text: {
@@ -14,12 +15,28 @@ var styles = ReactNative.StyleSheet.create({
     backgroundColor: 'white',
     fontSize: 30,
     margin: 80
+  },
+  container: {
+    flex: 1
   }
 });
 
+class HelloWorld extends React.Component {
+  render() {
+    return <ReactNative.Text style={styles.text}>Hello World (Again)</ReactNative.Text>;
+  }
+}
+
 class PropertyFinderApp extends React.Component {
   render() {
-    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
   }
 }
 
