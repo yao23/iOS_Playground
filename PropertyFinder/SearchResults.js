2bd9f47967ea27e03efe6ed5e2866b6fd048ba50
yao <myspiritcrazy@gmail.com>
2017-05-10 21:41:51 -0700

    add property details view

--- PropertyFinder/SearchResults.js
+++ PropertyFinder/SearchResults.js
@@ -9,6 +9,35 @@ import {
   ListView,
   Text
 } from 'react-native';
+var PropertyView = require('./PropertyView');
+
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
 
 class SearchResults extends Component {
 
@@ -21,12 +50,32 @@ class SearchResults extends Component {
     };
   }
 
+  rowPressed(listerURL) {
+    var property = this.props.listings.filter(prop => prop.lister_url === listerURL)[0];
+
+    this.props.navigator.push({
+      title: "Property",
+      component: PropertyView,
+      passProps: {property: property}
+    });
+  }
+
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.lister_url)}
           underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View  style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
