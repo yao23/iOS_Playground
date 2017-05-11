ce11db7f5ce257dd72df46f5a729c970a01dfcc0
yao <myspiritcrazy@gmail.com>
2017-05-10 19:58:57 -0700

    add house image

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -55,6 +55,10 @@ var styles = StyleSheet.create({
     borderColor: '#48BBEC',
     borderRadius: 8,
     color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
   }
 });
 
@@ -81,6 +85,7 @@ class SearchPage extends Component {
             underlayColor='#99d9f4'>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
+        <Image source={require('./Resources/house.png')} style={styles.image}/>
       </View>
     );
   }
