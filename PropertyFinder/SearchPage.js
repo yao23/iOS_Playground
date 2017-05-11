8b89b6b0a7ad8996816603ea9dc378f82cca84f2
yao <myspiritcrazy@gmail.com>
2017-05-10 19:50:52 -0700

    search input and button

--- PropertyFinder/SearchPage.js
+++ PropertyFinder/SearchPage.js
@@ -22,6 +22,39 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
   }
 });
 
@@ -35,6 +68,19 @@ class SearchPage extends Component {
         <Text style={styles.description}>
           Search by place-name, postcode or search near your location.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button}
+              underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight style={styles.button}
+            underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
       </View>
     );
   }
