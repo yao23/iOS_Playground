45d073560a58da886493e0353d4ea98de36a0ed4
yao <myspiritcrazy@gmail.com>
2017-05-11 23:44:15 -0700

    improve UI for item cell

--- ShopMercari/ShopMercari/ShopMercari/ItemCollectionViewCell.swift
+++ ShopMercari/ShopMercari/ShopMercari/ItemCollectionViewCell.swift
@@ -13,10 +13,34 @@ class ItemCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var soldImgView: UIImageView!
     @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var contentImgView: UIImageView!
 
     public func initData(item : Item) {
         titleLabel.text = item.name
         soldImgView.image = (item.status == "sold_out") ? UIImage(named: "sold") : nil
+//        self.insertSubview(soldImgView, aboveSubview: contentImgView)
         priceLabel.text = "$\(item.price)"
+        configImg(urlString: item.photo)
+        soldImgView.superview!.bringSubview(toFront: soldImgView)
+//        self.bringSubview(toFront: soldImgView)
+//        self.sendSubview(toBack: contentImgView)
+    }
+
+    public func configImg(urlString: String) {
+
+        URLSession.shared.dataTask(with: NSURL(string: urlString)! as URL, completionHandler: { (data, response, error) -> Void in
+
+            if error != nil {
+                print(error)
+                return
+            }
+            DispatchQueue.main.async(execute: { () -> Void in
+                let image = UIImage(data: data!)
+                self.contentImgView.image = image
+//                self.sendSubview(toBack: self.contentImgView)
+//                self.bringSubview(toFront: self.soldImgView)
+            })
+
+        }).resume()
     }
 }
