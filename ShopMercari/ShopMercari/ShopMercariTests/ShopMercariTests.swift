0c5a6edb203dde4d6034439f2571606bc6d099c7
yao <myspiritcrazy@gmail.com>
2017-05-13 22:28:32 -0700

    unit test uicollectionview has items

--- ShopMercari/ShopMercari/ShopMercariTests/ShopMercariTests.swift
+++ ShopMercari/ShopMercari/ShopMercariTests/ShopMercariTests.swift
@@ -53,4 +53,9 @@ class ShopMercariTests: XCTestCase {
 
         XCTAssertNotNil(systemUnderTest.collectionView)
     }
+
+    func testSUT_HasItemsForCollectionView() {
+
+        XCTAssertNotNil(systemUnderTest.items)
+    }
 }
